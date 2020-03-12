1acb590605d23ea5ce89308bec9817731224ab58
qian-xiaosheng <1326598206@qq.com>
2020-03-12 23:47:04 +0800

    mycat主从复制，读写分离demo

---
+++ src/main/java/com/yida/mycatdemo/MycatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yida.mycatdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycatDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycatDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yida/mycatdemo/MycatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yida.mycatdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycatDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
